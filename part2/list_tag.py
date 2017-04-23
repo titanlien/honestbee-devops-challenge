53d3d73ed182ebe992bd8bef81cbd5fac4afba10
Titan <titan.lien@gmail.com>
2017-04-23 21:16:44 +0800

    fix the split exception

--- part2/list_tag.py
+++ part2/list_tag.py
@@ -6,16 +6,16 @@ client = docker.from_env()
 containers = client.containers.list(filters={'status':'running'})
 
 
-print "CONTAINER ID\t", "TAG\t\t", "UP TO DATE?\t"
+print "CONTAINER ID\t", "TAG\t", "UP TO DATE?\t"
 for cont in containers:
     _id  = cont.attrs['Id'][:12]
-    name, tag = cont.attrs['Config']['Image'].split(':')
-    ## if we could not get tag, just assume it's latest
-    if tag is None:
-        ret = "FALSE\t"
-        tag = "latest"
-    elif tag == 'latest':
-        ret = "FALSE\t"
+    try:
+      name, tag = cont.attrs['Config']['Image'].split(':')
+      ## if we could not get tag, just assume it's latest
+    except ValueError:
+      tag = "latest"
+    if tag == 'latest':
+        ret = "FALSE"
     else:
-        ret ="TRUE\t"
-    print _id+"\t", tag+"\t\t", ret
+        ret ="TRUE"
+    print _id+"\t", tag+"\t", ret+"\t"
