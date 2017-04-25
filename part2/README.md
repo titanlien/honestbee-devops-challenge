985b580ea0b8629a27de19c725f9c45938927f3b
Titan <titan.lien@gmail.com>
2017-04-25 23:03:07 +0800

    put list_tag.py into docker image

--- part2/README.md
+++ part2/README.md
@@ -8,4 +8,8 @@ To use this example you will need an python virtual environment, and its require
 
 ## Usage
 
-./python list_tag.py
+### Put list_tag.py into python:2.7.13-alpine
+$ docker build . -t titanlien/list_tag
+
+### Launch list_tag.py with local docker socket
+$ docker run -ti --rm -v /var/run/docker.sock:/var/run/docker.sock titanlien/list_tag
--- part2/list_tag.py
+++ part2/list_tag.py
@@ -8,14 +8,19 @@ containers = client.containers.list(filters={'status':'running'})
 
 print "CONTAINER ID\t", "TAG\t", "UP TO DATE?\t"
 for cont in containers:
-    _id  = cont.attrs['Id'][:12]
-    try:
-      name, tag = cont.attrs['Config']['Image'].split(':')
-      image_id = cont.attrs['Image']
-      pull_image_id = client.images.pull(name + ':latest').id
-    ## if we could not get tag, just assume it's latest
-    except ValueError:
-      tag = "latest"
+  _id  = cont.attrs['Id'][:12]
+  try:
+    name, tag = cont.attrs['Config']['Image'].split(':')
+  ## if we could not get tag, just assume it's latest
+  except ValueError:
+    tag = "latest"
+  
+  try:
+    image_id = cont.attrs['Image']
+    pull_image_id = client.images.pull(name + ':latest').id
+  except:
+    pull_image_id = None
+  finally:
     if image_id == pull_image_id:
         ret = "FALSE"
     else:
